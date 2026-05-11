9610564d2b06220e48392d1ce659b0dfbac79577
Favour Uweja <uwejafavour@gmail.com>
2026-05-11 17:55:53 +0100

    www

--- src/main/kotlin/realtime_gateway/api/controller/TestController.kt
+++ src/main/kotlin/realtime_gateway/api/controller/TestController.kt
@@ -1,9 +1,11 @@
 package realtime_gateway.api.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/test")
 class TestController {
 
     @GetMapping("/ping")
